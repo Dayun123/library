5ef0bb37dc3220ac9165dd347236bc3146a94ee9
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-11-08 21:50:38 -0600

    Get the 'Hardcover Fiction' NYT Bestsellers List when you visit homepage.
    Added npm package 'bent' to the project in place of 'request' in
    order to hit the NYT Books API and pull data from their Best Seller
    lists onto the homepage. Not rendering anything yet, just got the
    request hooked up correctly.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bent = require('bent');
+
+const router = express.Router();
+
+/*
+  Endpoint construction:
+  Base_url: https://api.nytimes.com/svc/books/v3
+  Path: /lists.json
+  Query: ?list=hardcover-fiction
+  ApiKey: fg2mFVonHMDo4zdDgHvLu5VpWskgzVpK
+
+  Example:
+
+  https://api.nytimes.com/svc/books/v3/lists.json?list=hardcover-fiction&api-key=fg2mFVonHMDo4zdDgHvLu5VpWskgzVpK
+*/
+
+const apiKey = 'fg2mFVonHMDo4zdDgHvLu5VpWskgzVpK';
+
+const nytAPIRequest = bent('https://api.nytimes.com/svc/books/v3', 'json');
+const overviewPath = '/lists/overview.json'
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { pageTitle: 'Library' });
+router.get('/', async (req, res, next) => {
+
+  const nytBestSellers = await nytAPIRequest(`${overviewPath}?api-key=${apiKey}`);
+
+  const { books: hardcoverFictionList } = nytBestSellers.results.lists.find((list) => {
+    return list.list_name === 'Hardcover Fiction';
+  });
+  
+  res.json(hardcoverFictionList);
+  // res.render('index', { pageTitle: 'Library' });
 });
 
 module.exports = router;
