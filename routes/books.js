0204ce445ea4ecca103a7d27b909b2b408c6b2f3
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-11-10 16:18:29 -0600

    Pull relevant book info from Google Books API request results.
    The result set from the API call contains an array of books that
    match the search, but the first item in the array seems to be
    matching up correctly everytime so I'm going to simply extract
    that item from the list, without doing any checking to see if it
    actually matches the book I want. Down the line, may need to make
    this more robust by sending along some other informatin in the url,
    like the title of the book.

--- routes/books.js
+++ routes/books.js
@@ -2,18 +2,17 @@ const express = require('express');
 const bent = require('bent');
 
 const router = express.Router();
+
 const googleBooksApiBaseUrl = 'https://www.googleapis.com/books/v1/volumes';
 const apiJSONRequest = bent(googleBooksApiBaseUrl, 'json');
 
 router.get('/:isbn10', async (req, res, next) => {
   
   const query = `?q=${req.params.isbn10}`;
+  const { items } = await apiJSONRequest(query);
+  const bookInfo = items[0];
 
-  const results = await apiJSONRequest(query);
-
-  console.log(results);
-
-  res.json(req.params.isbn10);
+  res.json(bookInfo);
 });
 
 module.exports = router;
