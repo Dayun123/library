a0261a60b06ed3c81ee2cc13183f7e60a616281a
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-11-10 16:35:57 -0600

    Target isbn13 instead of isbn10 for /books/:isbn13 route.
    I'm going to use a 3rd api to pull book covers from, and it needs
    the isbn13 number as a search parameter.

--- routes/books.js
+++ routes/books.js
@@ -6,9 +6,9 @@ const router = express.Router();
 const googleBooksApiBaseUrl = 'https://www.googleapis.com/books/v1/volumes';
 const apiJSONRequest = bent(googleBooksApiBaseUrl, 'json');
 
-router.get('/:isbn10', async (req, res, next) => {
+router.get('/:isbn13', async (req, res, next) => {
   
-  const query = `?q=${req.params.isbn10}`;
+  const query = `?q=${req.params.isbn13}`;
   const { items } = await apiJSONRequest(query);
   const bookInfo = items[0];
 
