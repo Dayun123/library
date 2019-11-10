4e8c55f310dc1dc41d7382f3d8e82bdd9a9af9e3
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-11-10 16:11:20 -0600

    Get information from Google Books API at /books/:isbn10.
    For the /books/:isbn10 route, I'm going to pull the book data from
    the Google Books API, as there is more detailed information there
    than the NYT Books API. This is the reasoning behind sending the
    isbn10 info along in the url, it is easy to search for a book in
    the Google Books API using this number.

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,18 @@
 const express = require('express');
-const router = express.Router();
+const bent = require('bent');
 
-router.get('/:isbn10', (req, res, next) => {
+const router = express.Router();
+const googleBooksApiBaseUrl = 'https://www.googleapis.com/books/v1/volumes';
+const apiJSONRequest = bent(googleBooksApiBaseUrl, 'json');
+
+router.get('/:isbn10', async (req, res, next) => {
+  
+  const query = `?q=${req.params.isbn10}`;
+
+  const results = await apiJSONRequest(query);
+
+  console.log(results);
+
   res.json(req.params.isbn10);
 });
 
