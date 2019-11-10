1ca3845ab3f3aaeabeec8685edf76981157fe2b2
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-11-10 17:01:09 -0600

    Display book image at books/:isbn13 route.
    Still not confident in this method of displaying images, but it
    is a starting point at least. One of the images doesn't work
    when I query the Open Books API, so that's a problem. Maybe passing
    the image link from the bestseller's list on the homepage to the
    /books/:isbn13 route is the answer? That won't help for the more
    general search functionality down the road, but at least it will
    make consistency possible between the homepage and the bookpage.

--- routes/books.js
+++ routes/books.js
@@ -4,15 +4,20 @@ const bent = require('bent');
 const router = express.Router();
 
 const googleBooksApiBaseUrl = 'https://www.googleapis.com/books/v1/volumes';
-const apiJSONRequest = bent(googleBooksApiBaseUrl, 'json');
+const openLibraryApiBaseUrl = 'http://covers.openlibrary.org/b/ISBN/';
+const googleBooksApiJSONRequest = bent(googleBooksApiBaseUrl, 'json');
 
 router.get('/:isbn13', async (req, res, next) => {
   
   const query = `?q=${req.params.isbn13}`;
-  const { items } = await apiJSONRequest(query);
+  const { items } = await googleBooksApiJSONRequest(query);
   const bookInfo = items[0];
 
-  res.render('books', { pageTitle: bookInfo.volumeInfo.title });
+  // Only a thumbnail image comes with results from Google Books API. The Open Library API seems to have most book covers in large sizes, but not all.
+  bookInfo.imageLink = `${openLibraryApiBaseUrl}${req.params.isbn13}-L.jpg`;
+
+  res.render('books', { pageTitle: bookInfo.volumeInfo.title, bookInfo });
+
 });
 
 module.exports = router;
