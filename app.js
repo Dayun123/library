3b7304b0c0ce3bbdf38d2469c2a61e2e35c26f34
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-11-10 15:44:28 -0600

    Create a route handler for GET /logout.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login');
+const logoutRouter = require('./routes/logout');
 
 const app = express();
 
@@ -37,6 +38,7 @@ app.use((req, res, next) => {
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+app.use('/logout', logoutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
